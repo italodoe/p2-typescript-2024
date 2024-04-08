ab069ef62bf386b80725c7da33a05142aa159547
Italo Castillo <italogitdoe@gmail.com>
2024-04-08 21:52:30 +0200

    Add get trending

--- test-v2/src/main.ts
+++ test-v2/src/main.ts
@@ -1,3 +1,37 @@
-import './style.css'
+import "./style.css";
 // import typescriptLogo from './typescript.svg'
 
+(function () {
+  const tendingUrl = "https://api.giphy.com/v1/gifs/trending?";
+  const searchUrl = "https://api.giphy.com/v1/gifs/search?";
+  // var query;
+
+  const params = {
+    // q: query, //for search
+    // rating: pg, //rating: g, pg, pg-13
+    // offset: pg, //Default: “0” Maximum: “4999”
+    limit: 10,
+    api_key: "W93mPxjWAmjil8zKG0ZuBiaj3Pi8DRiO",
+    fmt: "json",
+    bundle: "messaging_non_clips",
+  };
+
+  const trending_button = document.getElementById("trending_button");
+  if (trending_button)
+    trending_button.addEventListener("click", getTrending, false);
+
+  async function callApiGetTrending() {
+
+    const body = await fetch(tendingUrl + new URLSearchParams(params))
+    const response = await body.json();
+    console.log("response--", response);
+
+
+
+  }
+
+  function getTrending() {
+ callApiGetTrending();
+
+  }
+})();
