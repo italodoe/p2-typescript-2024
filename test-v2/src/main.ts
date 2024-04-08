7a3e84af504437fe1720bfae6b1e45d509535e72
Italo Castillo <italogitdoe@gmail.com>
2024-04-08 22:05:51 +0200

    add seearch fetch

--- test-v2/src/main.ts
+++ test-v2/src/main.ts
@@ -4,10 +4,10 @@ import "./style.css";
 (function () {
   const tendingUrl = "https://api.giphy.com/v1/gifs/trending?";
   const searchUrl = "https://api.giphy.com/v1/gifs/search?";
-  // var query;
+  var query;
 
   const params = {
-    // q: query, //for search
+    //  q: query, //for search
     // rating: pg, //rating: g, pg, pg-13
     // offset: pg, //Default: “0” Maximum: “4999”
     limit: 10,
@@ -16,22 +16,38 @@ import "./style.css";
     bundle: "messaging_non_clips",
   };
 
+  // get trendings
   const trending_button = document.getElementById("trending_button");
   if (trending_button)
     trending_button.addEventListener("click", getTrending, false);
 
   async function callApiGetTrending() {
-
-    const body = await fetch(tendingUrl + new URLSearchParams(params))
+    delete params.q;
+    const body = await fetch(tendingUrl + new URLSearchParams(params));
     const response = await body.json();
     console.log("response--", response);
-
-
-
   }
 
   function getTrending() {
- callApiGetTrending();
-
+    callApiGetTrending();
   }
+
+  // get by search
+  const search_button = document.getElementById("search_button");
+  if (search_button)
+    search_button.addEventListener("click", getSearch, false);
+
+  async function callApiGetSearch() {
+    params.q = "rick and morty";
+    const body = await fetch(searchUrl + new URLSearchParams(params));
+    const response = await body.json();
+    console.log("response--", response);
+  }
+
+  function getSearch() {
+    callApiGetSearch();
+  }
+
+
+
 })();
