f2ac4417f6a4757bd7c59178450dedf00e7115b1
Italo Castillo <italogitdoe@gmail.com>
2024-04-11 20:33:09 +0200

    refactoring env file

--- project-v1/src/main.ts
+++ project-v1/src/main.ts
@@ -1,1 +1,24 @@
 import "./style.css";
+import { YOUTUBE_key } from "./env.ts";
+
+//youtube 
+const search_button = document.getElementById("search_button");
+const ytSearchUrl = "https://www.googleapis.com/youtube/v3/search?";
+const ytVideoEmbedUrl = "https://www.youtube.com/embed/";
+const app = document.getElementById("app");
+
+const yt_params = {
+  q:"",
+  key: YOUTUBE_key,
+  part: "snippet",
+  type: "video",
+  maxResults: 1,
+  videoEmbeddable: true
+};
+
+if (search_button)
+  search_button.addEventListener("click", searchHandler, false);
+
+function searchHandler(e: Event){
+  e.preventDefault;
+}
