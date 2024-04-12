b103c5be457ac21757c847557d2cae2dce62e524
Italo Castillo <italogitdoe@gmail.com>
2024-04-13 00:00:52 +0200

    add infinity scroll

--- project-v1/src/main.ts
+++ project-v1/src/main.ts
@@ -1,26 +1,27 @@
 import "./style.css";
 import { YOUTUBE_key } from "./env.ts";
-var interval;
+
 var isIntervalActive;
 var videoBgArrayIndex: number[] = [];
+const localKeyArray: string[] = [];
+var ytItemsArray: JSON[] = [];
+const ytMaxCalls = 5; //max reloads for the same query  
 
 //header effects
-const changeBackgroundVideo = () => {
-  interval = setInterval(() => {
-    isIntervalActive = true;
-    const videoBg = document.getElementById("header_back_video");
-    const videoSource = videoBg?.dataset.source;
-    const videoMaxIndex = videoBg?.dataset.maxindex;
+const changeBackgroundVideo = setInterval(() => {
+  isIntervalActive = true;
+  const videoBg = document.getElementById("header_back_video");
+  const videoSource = videoBg?.dataset.source;
+  const videoMaxIndex = videoBg?.dataset.maxindex;
 
-    if (videoMaxIndex) {
-      if (videoBgArrayIndex.length === 0)
-        videoBgArrayIndex = Array.from(Array(parseInt(videoMaxIndex)).keys());
-      videoBgArrayIndex.sort(() => 0.5 - Math.random());
-      videoBg.src = videoSource + String(videoBgArrayIndex.pop()) + ".mp4";
-      console.log(videoBg.src);
-    }
-  }, 8000);
-};
+  if (videoMaxIndex) {
+    if (videoBgArrayIndex.length === 0)
+      videoBgArrayIndex = Array.from(Array(parseInt(videoMaxIndex)).keys());
+    videoBgArrayIndex.sort(() => 0.5 - Math.random());
+    videoBg.src = videoSource + String(videoBgArrayIndex.pop()) + ".mp4";
+    console.log("changeBackgroundVideo", videoBg.src);
+  }
+}, 6000);
 
 function setSearchedFlag() {
   const searchClassName = "searched";
@@ -28,7 +29,9 @@ function setSearchedFlag() {
   bg?.classList.add(searchClassName);
 }
 
-//youtube
+/**
+ * Youtube
+ */
 const search_form = document.getElementById("search_form");
 const search_button = document.getElementById("search_button");
 const search_input = document.getElementById("search_input");
@@ -36,46 +39,74 @@ const youtubeSearchUrl = "https://www.googleapis.com/youtube/v3/search?";
 const videoEmbedUrl = "https://www.youtube.com/embed/";
 const app = document.getElementById("app");
 
+//global params
 const yt_params = {
   q: "",
   key: YOUTUBE_key,
   part: "snippet",
   type: "video",
-  maxResults: 50,
+  maxResults: 10,
   videoEmbeddable: true,
   pageToken: undefined,
 };
+localKeyArray.push("q");
+localKeyArray.push("next_page_token");
+localKeyArray.push("max_token_call");
 
 if (search_form) search_form.addEventListener("submit", searchHandler, false);
 
 async function searchHandler(e: Event) {
   console.log(e);
   e.preventDefault();
-  yt_params.q = search_input?.value;
-  const response = await youtubeSearchApi();
-  const nextPageToken = response.nextPageToken;
-  const response2 = await youtubeSearchApi(nextPageToken);
-  
-  console.log("response2", response2);
-
-  addThumbnails(response);
-  addThumbnails(response2, false);
+  const _q = search_input?.value;
+  search(_q);
 }
 
-async function youtubeSearchApi(nextPageToken = null) {
-  delete yt_params.pageToken;
-  if (nextPageToken) {
-    yt_params.pageToken = nextPageToken;
+async function search(q = null) {
+  let _q: string;
+  if (q) {
+    clearLocalStorage();
+    _q = q;
+  } 
+  else {
+    _q = getLocalStorageItem("q");
   }
+  const maxTokenCall = getLocalStorageItem("max_token_call");
+  let _maxTokenCallInt = maxTokenCall ? parseInt(maxTokenCall) : -1;
+
+  if (_q && (ytMaxCalls >= _maxTokenCallInt)) {
+    console.log('inside');
+    yt_params.q = _q;
+    delete yt_params.pageToken;
+    const _nextPageToken = getLocalStorageItem("next_page_token");
+
+    let clear = true;
+    if (_nextPageToken) {
+      yt_params.pageToken = _nextPageToken;
+      clear = false;
+    }
+    const response = await youtubeSearchApi();
+    //todo check response
+    setLocalStorageItem("next_page_token", response.nextPageToken);
+    setLocalStorageItem("q", _q);
+    setLocalStorageItem("max_token_call", ++_maxTokenCallInt);
+    addThumbnails(response.items, clear);
+    return true;
+  }
+  return false;
+}
+
+async function youtubeSearchApi() {
   const body = await fetch(youtubeSearchUrl + new URLSearchParams(yt_params));
   const response = await body.json();
   console.log("response--", response);
-
   return response;
 }
 
-const addThumbnails = (response, clear = true) => {
-  const itemsArray = response.items;
+const addThumbnails = (items: Array<JSON>, clear = true) => {
+  console.log("addThumbnails", items);
+
+  const itemsArray = (ytItemsArray = items);
   if (clear) clearApp();
 
   itemsArray.forEach(function (video, index) {
@@ -143,5 +174,50 @@ function embed(video) {
 function clearApp() {
   if (app) app.textContent = "";
 }
+(function () {
+  clearLocalStorage();
+  changeBackgroundVideo;
+})();
 
-changeBackgroundVideo();
+/**
+ * Infinity Scroll
+ */
+window.addEventListener("scroll", infinityScrollHandler, false);
+
+async function infinityScrollHandler(e){
+  const { clientHeight, scrollHeight, scrollTop } = document.documentElement;
+  console.log({ clientHeight, scrollHeight, scrollTop });
+  const offset = 2; //px
+  if (scrollTop + clientHeight > scrollHeight - offset) {
+    const copyright_sym = document.getElementById("copyright_sym");
+    copyright_sym?.classList.add("searching");
+    const finished = await search();
+    console.log("finished", finished);
+    copyright_sym?.classList.remove("searching");
+  }
+}
+
+
+/**
+ * Local Storage Management
+ */
+
+function setLocalStorageItem(key: string, value: any) {
+  window.localStorage.setItem(key, value);
+}
+
+function getLocalStorageItem(key: string) {
+  return localStorage.getItem(key);
+}
+
+function clearLocalStorage(all = false, key = null) {
+  console.log(localKeyArray);
+
+  if (all) localStorage.clear();
+  else {
+    if (key) localStorage.removeItem(key);
+    else {
+      localKeyArray.forEach((item) => localStorage.removeItem(item));
+    }
+  }
+}
